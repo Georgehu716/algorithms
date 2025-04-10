7aacf02adfecf0eb571cd22ab2fa9b49fdd665eb
georgehu716 <2992786775@qq.com>
2025-04-10 16:06:21 +0800

    update unique_paths

--- data-structures-and-algorithms/dynamic-programming/unique_path.py
+++ data-structures-and-algorithms/dynamic-programming/unique_path.py
@@ -24,6 +24,10 @@ class Solution:
         return helper(m, n)
 
     def unique_paths_memorization(self, m, n):
+        """
+        TIME: O(m * n)
+        SPACE: O(m * n) [but limited to max call stack size and susceptible to overflow]
+        """
         dp = [[0 for _ in range(n + 1)] for _ in range(m + 1)]
 
         def helper(m, n):
@@ -35,3 +39,29 @@ class Solution:
 
             dp[m][n] = helper(m-1, n) + helper(m, n-1)
             return dp[m][n]
+
+    def unique_paths_iteration_and_tabulation(self, m, n):
+        """
+        TIME: O(n * m)
+        SPACE: O(n * m)
+        """
+        dp = [[1 for _ in range(n)] for _ in range(m)]
+
+        for r in range(len(dp) - 2, -1, -1):
+            for c in range(len(dp[r]) - 2, -1, -1):
+                dp[r][c] = dp[r+1][c] + dp[r][c+1]
+
+        return dp[0][0]
+
+    def unique_paths_iteration_and_tabulation_with_space_optimized(self, m, n):
+        """
+        TIME: O(n * m)
+        SPACE: O(n)
+        """
+        dp = [[1 for _ in range(n)] for _ in range(2)]
+
+        for r in range(m - 2, -1, -1):
+            for c in range(n - 2, -1, -1):
+                dp[0][c] = dp[1][c] + dp[0][c+1]
+            dp[1] = dp[0]
+        return dp[0][0]
